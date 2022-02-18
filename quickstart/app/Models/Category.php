4bc0884b470f030f5687cbbfea06f7700383b14f
aries <hathanhhoang842000@gmail.com>
2022-02-18 08:37:15 +0700

    created model, controller, migrate, route

---
+++ quickstart/app/Models/Category.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+    protected $table = "categories";
+    protected $filltable = [
+        'cat_name',
+        'cat_desc',
+    ];
+
+    /**
+     * Get all of the product for the Category
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
+}
